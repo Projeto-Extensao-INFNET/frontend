5346df2e24f776b4e9febe24e8ca9fc4e2155070
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-29 17:18:20 -0300

    feat: install and setup tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
 export const App = () => {
-  return <h1>Hello World</h1>;
+  return <h1 className="text-green-500">Hello World</h1>;
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import path from "node:path";
-import react from '@vitejs/plugin-react'
+import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    open:true
+  plugins: [react(), tailwindcss()],
+  server: {
+    open: true,
   },
-  resolve:{
-    alias:{
-      "@":path.resolve(__dirname,'src')
-    }
-  }
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
