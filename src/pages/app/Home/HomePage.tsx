4e826968256c4cb08cdb84dd7bc4b9de55ea90d8
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-16 16:26:48 -0300

    fix(HomePage): move useHomeModel hook inside HomePage component

--- src/pages/app/Home/HomePage.tsx
+++ src/pages/app/Home/HomePage.tsx
@@ -2,11 +2,9 @@
 import { useHomeModel } from './HomeModel';
 import { HomeView } from './HomeView';
 
-// Executa o Model e pega os dados/métodos
-// biome-ignore lint/correctness/useHookAtTopLevel: i'll fix this soon
-const methods = useHomeModel();
-
 export const HomePage = () => {
-  // Passa todos os dados/métodos para a View via props
+  // Executa o Model e pega os dados/métodos
+  const methods = useHomeModel();
+
   return <HomeView {...methods} />;
 };
