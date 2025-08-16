855730c768187c3a3a824095da4452cc0b4dd450
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-16 15:14:11 -0300

    feat(Home): add initial logic on HomeView component

--- src/pages/app/Home/HomeView.tsx
+++ src/pages/app/Home/HomeView.tsx
@@ -1,4 +1,12 @@
-export const HomeView = () => {
+// View é onde fica toda a UI
+import type { useHomeModel } from './HomeModel';
+
+// type que define a tipagem das props baseado no retorno da Model
+type HomeViewProps = ReturnType<typeof useHomeModel>;
+
+// apenas renderiza a UI usando os dados vindos das props
+// biome-ignore lint/correctness/noEmptyPattern: i'll fix this soon
+export const HomeView = ({}: HomeViewProps) => {
   return (
     <div>
       <h1>Futura Página Inicial</h1>
