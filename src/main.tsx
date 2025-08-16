b5f714e3e1664295dcdae01a2dc3965b7bbab800
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-16 11:56:44 -0300

    refactor: update import path for enableMSW

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { enableMSW } from '@/services/api/mocks';
 import { App } from './App.tsx';
-import { enableMSW } from './api/mocks/index.ts';
 
 enableMSW().then(() => {
   // biome-ignore lint/style/noNonNullAssertion: mandatory by React
