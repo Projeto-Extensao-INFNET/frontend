a49c18ad12ff9a13013eea729c4edf6146bfbce2
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-16 16:23:34 -0300

    feat: add HttpClientContract

---
+++ src/infra/http/HttpClientContract.ts
@@ -0,0 +1,24 @@
+// Enum que define os métodos HTTP
+export enum HttpMethod {
+  GET = 'get',
+  POST = 'post',
+  PUT = 'put',
+  PATCH = 'patch',
+  DELETE = 'delete',
+}
+
+// Tipo genérico para requisições HTTP
+export type HttpRequest<TBody> = {
+  endpoint: string; // endpoint da API (ex: /users)
+  method: HttpMethod; // método HTTP (ex: GET,POST etc)
+  body?: TBody; // Body da requisição (opcional)
+  headers?: Record<string, string>; // Headers da requisição (opcional)
+};
+
+// Interface que define o contrato do HttpClient
+export interface IHttpClient {
+  // método para enviar requisições
+  sendRequest: <TResponse, TBody = unknown>(
+    request: HttpRequest<TBody>
+  ) => Promise<TResponse>;
+}
