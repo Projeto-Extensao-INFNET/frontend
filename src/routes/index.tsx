f2dd3fe49fcf5a5442d8671389169810e06f7889
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-16 13:29:59 -0300

    feat(navigation): add initial HomeView and SignInView routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { AppLayout } from '@/pages/_layouts/app';
 import { AuthLayout } from '@/pages/_layouts/auth';
+import { HomeView } from '@/pages/app/Home/HomeView';
+import { SignInView } from '@/pages/auth/SignIn/SignInView';
 
-export const RoutesPath = {} as const;
+export const RoutesPath = {
+  SignIn: '/signIn',
+} as const;
 
 export const Navigation = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AppLayout />}>
-          {/* <Route element={<DashboardView} index/> */}{' '}
-          {/* Futura página inicial */}
+          <Route element={<HomeView />} index />
         </Route>
 
         <Route element={<AuthLayout />}>
-          {/* <Route element={<SignInView/>} path={RoutesPath.signIn}/> */}
+          <Route element={<SignInView />} path={RoutesPath.SignIn} />
         </Route>
       </Routes>
     </BrowserRouter>
