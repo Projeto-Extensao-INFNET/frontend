3bf376c052d79b6fdf899ff0d4de7761ff20370d
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-11-12 17:21:05 -0300

    feat: install and setup React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { App } from './App.tsx';
 import { enableMSW } from './api/mocks/index.ts';
+import { client } from './lib/react-query.ts';
 
 enableMSW().then(() => {
   // biome-ignore lint/style/noNonNullAssertion: mandatory by React
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
-      <App />
+      <QueryClientProvider client={client}>
+        <App />
+      </QueryClientProvider>
     </StrictMode>
   );
 });
