751d780804e7640e4bbb5742b1f6acb52fa234a3
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-03-02 10:05:44 -0300

    feat: add custom render function and update Vitest configuration

--- vitest.global.setup.ts
+++
@@ -1,7 +0,0 @@
-export async function setup() {
-  // Roda antes de todos os testes
-}
-
-export async function teardown() {
-  // Roda depois de todos os testes
-}
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,13 +1,1 @@
-/** biome-ignore-all lint/performance/noNamespaceImport: mandatory by react testing library */
-import { cleanup } from '@testing-library/react';
-import { afterEach, expect, vi } from 'vitest';
-import '@testing-library/jest-dom/vitest';
-import * as matchers from '@testing-library/jest-dom/matchers';
-
-expect.extend(matchers);
-
-afterEach(() => {
-  cleanup();
-
-  vi.resetAllMocks();
-});
+import '@testing-library/jest-dom';
