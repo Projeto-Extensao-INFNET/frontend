1a4a554c064b624b5887a838a59e5aa874ba34bc
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-03-02 10:15:25 -0300

    feat: set up routing with React Router and define base route

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import { QueryClientProvider } from '@tanstack/react-query';
 import { App } from './App.tsx';
-import { enableMSW } from './api/mocks/index.ts';
-import { client } from './lib/react-query.ts';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { client } from '@lib/react-query.ts';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import '@styles/index.css';
+import { ROUTES_PATH } from './shared/constants/routes.ts';
 
-enableMSW().then(() => {
-  // biome-ignore lint/style/noNonNullAssertion: mandatory by React
-  createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <QueryClientProvider client={client}>
-        <App />
-      </QueryClientProvider>
-    </StrictMode>
-  );
-});
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={ROUTES_PATH.BASE} element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>,
+);
