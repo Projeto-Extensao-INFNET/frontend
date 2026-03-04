b40ccf7aae79fb7a6cd25548862431b45378e308
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-04 17:49:52 -0300

    feat: add cleanup function to vitest setup for improved test isolation

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,9 @@
 import '@testing-library/jest-dom';
-import { beforeAll, afterAll, afterEach } from 'vitest';
+import { beforeAll, afterAll, afterEach, beforeEach } from 'vitest';
+import { cleanup } from './src/__tests__/custom-render';
 import { server } from './src/__mocks__/node';
 
+beforeEach(() => cleanup());
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
