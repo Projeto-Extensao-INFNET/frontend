e5d5241ca4b185db02d168b61c15fa9c57056520
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-04 18:13:59 -0300

    feat: enhance custom render with QueryClientProvider wrapper

--- src/__tests__/custom-render.tsx
+++ src/__tests__/custom-render.tsx
@@ -1,11 +1,18 @@
-import type { ReactElement } from 'react';
+import type { FC, ReactElement, ReactNode } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const Wrapper: FC<{ children: ReactNode }> = ({ children }) => {
+  const client = new QueryClient();
+
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+};
 
 export function CustomRender(
   ui: ReactElement,
   options?: Omit<RenderOptions, 'wrapper'>,
 ) {
-  return render(ui, { ...options });
+  return render(ui, { ...options, wrapper: Wrapper });
 }
 
 export * from '@testing-library/react';
