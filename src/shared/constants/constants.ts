2e518bed114c35e48e1a07b10025fbc6d8679786
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-16 17:33:45 -0300

    feat: add ACCOUNT_TYPES and DOCUMENT_TYPES constants

---
+++ src/shared/constants/constants.ts
@@ -0,0 +1,9 @@
+export const ACCOUNT_TYPES = [
+  { id: 1, role: 'PATIENT', value: 'Paciente' },
+  { id: 2, role: 'PROFESSIONAL', value: 'Profissional' },
+];
+
+export const DOCUMENT_TYPES = [
+  { id: 1, value: 'CPF' },
+  { id: 2, value: 'RG' },
+];
