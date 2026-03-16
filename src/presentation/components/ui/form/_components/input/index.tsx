b163b5d66f4000f74039a173ae38468a1b768744
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-16 17:33:29 -0300

    refactor: refactor Input component to use forwardRef for better integration with React Hook Form

--- src/presentation/components/ui/form/_components/input/index.tsx
+++ src/presentation/components/ui/form/_components/input/index.tsx
@@ -1,35 +1,47 @@
-import type { FC, InputHTMLAttributes } from 'react';
+import { forwardRef, type InputHTMLAttributes } from 'react';
 import { ShowPassword } from '../show-password';
-import { useInputModel } from '../../input.model';
+import { useInputModel } from './input.model';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-export const Input: FC<InputProps> = ({
-  className,
-  onFocus,
-  onBlur,
-  value,
-  onChange,
-  placeholder,
-  type,
-  ...props
-}) => {
-  const { handlePasswordToggle, showPassword } = useInputModel();
-  return (
-    <div className="relative flex w-80 items-center gap-2">
-      <input
-        placeholder={placeholder}
-        className="w-full rounded-md border border-black bg-white px-2 py-3.5"
-        type={type === 'password' && showPassword ? 'text' : type}
-        {...props}
-      />
+// forwardRef => envia a ref do input para o React Hook Form
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      className,
+      onFocus,
+      onBlur,
+      value,
+      onChange,
+      placeholder,
+      type,
+      ...props
+    },
+    ref,
+  ) => {
+    const { handlePasswordToggle, showPassword } = useInputModel();
 
-      {type === 'password' && (
-        <ShowPassword
-          onClick={handlePasswordToggle}
-          showPassword={showPassword}
+    return (
+      <div className="relative flex w-80 items-center gap-2">
+        <input
+          ref={ref}
+          placeholder={placeholder}
+          className="w-full rounded-md border border-black bg-white px-2 py-3.5"
+          type={type === 'password' && showPassword ? 'text' : type}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          value={value}
+          onChange={onChange}
+          {...props}
         />
-      )}
-    </div>
-  );
-};
+
+        {type === 'password' && (
+          <ShowPassword
+            onClick={handlePasswordToggle}
+            showPassword={showPassword}
+          />
+        )}
+      </div>
+    );
+  },
+);
