bbed24674a29b6eefaaabbd275b49886e2fc6f95
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-16 17:30:38 -0300

    feat: install shadcnUI button, calendar, date-picker, and popover components

---
+++ src/presentation/components/ui/date-picker.tsx
@@ -0,0 +1,46 @@
+import { useState, type FC } from 'react';
+import { Button } from '@presentation/components/ui/button';
+import { Calendar } from '@presentation/components/ui/calendar';
+import { Form } from '@presentation/components/ui/form';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@presentation/components/ui/popover';
+
+interface DatePickerProps {
+  value?: Date;
+  onChange?: (date: Date | undefined) => void;
+}
+
+export const DatePicker: FC<DatePickerProps> = ({ value, onChange }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Form.Field className="w-full">
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            id="date"
+            className="justify-start font-normal"
+          >
+            {value ? value.toLocaleDateString() : 'Select date'}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+          <Calendar
+            mode="single"
+            selected={value}
+            defaultMonth={value}
+            captionLayout="dropdown"
+            onSelect={(date) => {
+              onChange?.(date);
+              setOpen(false);
+            }}
+          />
+        </PopoverContent>
+      </Popover>
+    </Form.Field>
+  );
+};
