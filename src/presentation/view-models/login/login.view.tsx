bb20445a3198c3a88269ac82e519b29aca4144e4
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-18 18:48:00 -0300

    feat: implement login view with form handling and navigation

--- src/presentation/view-models/login/login.view.tsx
+++ src/presentation/view-models/login/login.view.tsx
@@ -1,6 +1,73 @@
 import type { FC } from 'react';
 import type { useLoginModel } from './useLoginModel';
+import { Form } from '@/presentation/components/ui/form';
+import { PageTitle } from '@/presentation/components/page-title';
+import { Button } from '@/presentation/components/ui/button';
 
-export const LoginView: FC<ReturnType<typeof useLoginModel>> = () => {
-  return <h1>LOGIN</h1>;
+export const LoginView: FC<ReturnType<typeof useLoginModel>> = ({
+  register,
+  isPending,
+  errors,
+  onSubmit,
+  navigateToRegister,
+}) => {
+  return (
+    <main>
+      <PageTitle>Login</PageTitle>
+      <section className="flex items-center justify-center">
+        <div className="mt-12">
+          <Form.Container>
+            <Form.Root onSubmit={onSubmit}>
+              <Form.Label label="E-mail" />
+              <Form.Field>
+                <Form.Input
+                  type="email"
+                  placeholder="informe seu e-mail"
+                  {...register('email')}
+                />
+                {errors.email && (
+                  <Form.Error errorMessage={errors.email.message} />
+                )}
+              </Form.Field>
+
+              <Form.Label label="Senha" />
+              <Form.Field>
+                <Form.Input
+                  type="password"
+                  placeholder="informe sua senha"
+                  {...register('password')}
+                />{' '}
+                {errors.password && (
+                  <Form.Error errorMessage={errors.password.message} />
+                )}
+              </Form.Field>
+
+              <div className="mb-5 flex h-full flex-col items-center justify-center pt-4">
+                <div className="space-y-2">
+                  {' '}
+                  <Button
+                    type="submit"
+                    size={'lg'}
+                    disabled={isPending}
+                    className="min-w-full"
+                  >
+                    {isPending ? 'Acessando...' : 'Acessar'}
+                  </Button>
+                  <Button
+                    type="button"
+                    variant={'secondary'}
+                    size={'lg'}
+                    className="min-w-full"
+                    onClick={navigateToRegister}
+                  >
+                    Ainda não possui conta? Cadastrar!
+                  </Button>
+                </div>
+              </div>
+            </Form.Root>
+          </Form.Container>
+        </div>
+      </section>
+    </main>
+  );
 };
