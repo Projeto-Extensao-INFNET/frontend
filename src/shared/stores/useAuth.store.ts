f7bc4e08d43e7f415afecd36cbda5b3de08c74ba
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-18 18:49:05 -0300

    chore: add TODO comments

--- src/shared/stores/useAuth.store.ts
+++ src/shared/stores/useAuth.store.ts
@@ -1,41 +1,36 @@
 import { create } from 'zustand';
-import { createJSONStorage, persist } from 'zustand/middleware';
 
-interface User {
-  userId: string;
+// ?? [ ] => criar possível método de logout e remover a rota de logout do backend?
+// TODO [ ] => encontrar uma forma segura de acessar os dados presentes nos cookies
+// ?? [ ] => implementar logout automático quando o token ficar inválido?
+// !FIX [ ] => corrigir o recebimento dos dados do usuário vindos do backend
+
+export interface User {
   username: string;
+  sub: string;
+  role: string;
 }
 
-type UpdateTokensParams = {
-  accessToken: string | null;
-};
+interface Session {
+  accessToken: string;
+  refreshToken: string;
+  data: {
+    user: User;
+  };
+}
 
 interface States {
   user: User | null;
-  accessToken: string | null;
 }
 
 interface Actions {
-  logout: () => void;
-  updateTokens: (updateTokensParams: UpdateTokensParams) => void;
+  setSession: (sessionData: Session) => void;
 }
 
 type AuthStore = States & Actions;
 
-export const useAuthStore = create<AuthStore>()(
-  persist(
-    (set) => ({
-      user: null,
-      accessToken: null,
+export const useAuthStore = create<AuthStore>()((set) => ({
+  user: null,
 
-      logout: () =>
-        set({
-          user: null,
-          accessToken: null,
-        }),
-
-      updateTokens: (updateTokensParams) => set({ ...updateTokensParams }),
-    }),
-    { name: 'auth', storage: createJSONStorage(() => localStorage) },
-  ),
-);
+  setSession: (sessionData) => set({ user: sessionData.data.user }),
+}));
