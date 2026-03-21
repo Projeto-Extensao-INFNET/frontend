ae0817f20bff5ed5f479e99c0af1561b0d0d3b59
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-21 13:13:01 -0300

    chore: files replaced

--- src/presentation/components/ui/form/_components/container/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC, ReactNode } from 'react';
-
-interface ContainerProps {
-  children: ReactNode;
-}
-
-export const Container: FC<ContainerProps> = ({ children }) => {
-  return <div className="flex flex-col">{children}</div>;
-};
--- src/presentation/components/ui/form/_components/error/index.tsx
+++
@@ -1,3 +0,0 @@
-export const Error = ({ errorMessage }: { errorMessage?: string }) => {
-  return <p className="text-xs text-red-500">{errorMessage}</p>;
-};
