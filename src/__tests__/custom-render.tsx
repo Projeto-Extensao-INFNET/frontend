143a5beda13a56ecf1295d81c697da7ce8e2f12d
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-03-21 21:56:22 -0300

    refactor: update CustomRender function to accept a broader UI type and options interface

--- src/__tests__/custom-render.tsx
+++ src/__tests__/custom-render.tsx
@@ -2,17 +2,20 @@ import type { FC, ReactElement, ReactNode } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+type UI = ReactElement | string;
+
+interface Options extends Omit<RenderOptions, 'wrapper'> {
+  props?: Record<string, unknown>;
+}
+
 export const Wrapper: FC<{ children: ReactNode }> = ({ children }) => {
   const client = new QueryClient();
 
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 };
 
-export function CustomRender(
-  ui: ReactElement,
-  options?: Omit<RenderOptions, 'wrapper'>,
-) {
-  return render(ui, { ...options, wrapper: Wrapper });
+export function CustomRender(ui: UI, options: Options) {
+  return render(ui as ReactElement, options);
 }
 
 export * from '@testing-library/react';
