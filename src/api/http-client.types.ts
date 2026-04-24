138c4d7c7eb6a2ad318a3c484edf22bc1fd388e9
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-04-24 20:11:32 -0300

    feat: add params to http request type

--- src/api/http-client.types.ts
+++ src/api/http-client.types.ts
@@ -1,16 +1,17 @@
 export enum HttpMethod {
-  GET = "get",
-  POST = "post",
-  PUT = "put",
-  PATCH = "patch",
-  DELETE = "delete",
+  GET = 'get',
+  POST = 'post',
+  PUT = 'put',
+  PATCH = 'patch',
+  DELETE = 'delete',
 }
 
 export type HttpRequest<TBody> = {
   endpoint: string;
   method: HttpMethod;
-  body?: TBody;
   headers?: Record<string, string>;
+  body?: TBody;
+  params?: Record<string, unknown>;
 };
 
 export type IHttpClient = {
